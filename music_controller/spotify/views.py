b50bfb3024492ed89b6b830e1adcb17a8ae7b21f
CK <movementfor@gmail.com>
2023-04-15 09:25:09 +0100

    Fixed Spotify api connection issues

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -26,24 +26,24 @@ def spotify_callback(request, format=None):
     code = request.GET.get('code')
     error = request.GET.get('error')
 
-    response = post('https://accounts/spotify.com/api/token', data={
+    response = post('https://accounts.spotify.com/api/token', data={
         'grant_type': 'authorization_code',
         'code': code,
         'redirect_uri': REDIRECT_URI,
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET
     }).json()
-
+    print(response)
     access_token = response.get('access_token')
     token_type = response.get('token_type')
     refresh_token = response.get('refresh_token')
-    expires_in = response.get('expires_in')
+    expires_in = response.get('expires_in')    
     error = response.get('error')
 
-    if not request.session.exists(request.session.sessions_key):
+    if not request.session.exists(request.session.session_key):
         request.session.create()
 
-    update_or_create_user_tokens(request.session.sessions_key, access_token, token_type, expires_in, refresh_token)
+    update_or_create_user_tokens(request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
     return redirect('frontend:')
     
